29f6a2e4cec69d14e082f7d4cbc7e1afa2f5351e
NikhilReddy018 <nikhil2008reeddy898@gmail.com>
2025-07-01 00:14:58 +0530

    assignment operators

---
+++ Practice_1.c
@@ -0,0 +1,12 @@
+// as value to variable
+#include<stdio.h>
+void main()
+{
+    float perc;
+    printf("Enter the perc");
+    scanf("%f",&perc);
+    printf("perc values is %f:",perc);
+    perc=0.18/100;
+    scanf("%f",&perc);
+    
+}
